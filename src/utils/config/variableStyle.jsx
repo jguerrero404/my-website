593fc60ec57b43c55b4ba74d7200343074366afe
Jose Guerrero <josealberto.guerreroayala@gmail.com>
2019-08-21 21:59:25 -0500

    reboot de estilos y variables css creados

---
+++ src/utils/config/variableStyle.jsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components"
+
+export const VariableStyle = createGlobalStyle`
+:root {
+--sidenav-width: 70px;
+--sidenav-max-width: 70px;
+--sidenav-bg: var(--gray-5);
+--sidenav-shadow: 4px 0 10px -3px #010101;
+--sidenav-border:2px dashed var(--text-color);
+
+--card-bg: var(--gray-5);
+--card-shadow: 0 4px 8px rgba(0, 0, 0, 0, 0.38);
+
+--max-width-bp:768px;
+--background:var(--gray6);
+--text-color:var(--gray2);
+--h-color:#fff;
+
+--card-shadow:0 4px 8px rgba(0,0,0,0.38);
+--toc-shadow:rgba(0,0,0,0.7) 0px 10px 20px 0px;
+--tag-bg:var(--gray4);
+--code-bg:#22262f;
+--card-bg:var(--gray5);
+--overlay-bg:rgba(0,0,0,0.9);
+--h-border:2px dashed var(--sidenav-bg);
+transition:all .3s ease
+
+}
+`
